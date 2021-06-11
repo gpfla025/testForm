a84de73cfed47781b311f334a9817a6866644dac
hyelim.shim <76678363+gpfla025@users.noreply.github.com>
2021-06-12 01:51:31 +0900

    Update form.js

--- form.js
+++ form.js
@@ -1,7 +1,10 @@
 function testGS(){
 
-  const url = "https://script.google.com/macros/s/AKfycbz2gw2BbfXm0JAoSnlacGAFLK4AJJCEFsLLXleJWJeC-ey5SX5vxUIC4HCeDQkFfL98/exec"
+  const url = "https://script.google.com/macros/s/AKfycbyxCQSmNV77AKeI8lmTx_nUdFHHtXlCb1uhV0P1-dbwQH3nBOSisXP1ATWDMSeqGnbO/exec"
 
+
+
+  
   fetch(url)
     .then(d => d.json())
     .then(d => {
@@ -12,4 +15,25 @@ function testGS(){
 
 }
 
+function addGS(){
+
+  const url = "https://script.google.com/macros/s/AKfycbyxCQSmNV77AKeI8lmTx_nUdFHHtXlCb1uhV0P1-dbwQH3nBOSisXP1ATWDMSeqGnbO/exec"
+
+  fetch(url, {
+    method: 'POST',
+    mode: 'no-cors',
+    cache: 'no-cache',
+    credentials: 'omit',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    //referrerPolicy: 'no-referrer',
+    body: JSON.stringify({name:"Esther"})
+  });
+
+}
+
 document.getElementById("btn").addEventListener("click", testGS);
+
+document.getElementById("btn2").addEventListener("click", addGS);
