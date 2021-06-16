ae8c87e1572fc69ea3c7d0e1f4ff1d5c27ff5302
hyelim.shim <76678363+gpfla025@users.noreply.github.com>
2021-06-16 23:02:15 +0900

    Update customers.js

--- customers.js
+++ customers.js
@@ -23,7 +23,7 @@ function addGS(){
     //credentials: 'omit',
     headers: { 'Content-Type': 'application/json' },
     redirect: 'follow',
-    body: JSON.stringify({first:"Hyelim,",last:"Shim",phone:"010-0505-1234"})
+    body: JSON.stringify({first:"Hyelim",last:"Shim",phone:"010-0505-1234"})
   });
 
 }
