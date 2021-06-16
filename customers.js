f406d79c60955d0f52559115a65e0a83b322ca0c
hyelim.shim <76678363+gpfla025@users.noreply.github.com>
2021-06-17 01:42:53 +0900

    Update customers.js

--- customers.js
+++ customers.js
@@ -1,33 +1,72 @@
-function testGS(){
-    const url = "https://script.google.com/macros/s/AKfycbzn-vjkl4BHUGJY8_k_S95cPlzVRkvyOEFOL6YoFwuX-Ryl5HSv8ixrTmqpmFjcn5Sl/exec"
+var customerForm = document.getElementById("customerForm");
+var firstName = document.getElementById("Name");
+var lastName = document.getElementById("Idnumber");
+var phoneNumber = document.getElementById("PhoneNumber");
+var city = document.getElementById("Address");
+var submitButton = document.getElementById("submitButton");
+var buttonSpinner = document.getElementById("buttonSpinner");
+var buttonText = document.getElementById("buttonText");
+var unknownError = document.getElementById("unknownError");
 
-    fetch(url)
-      .then(d => d.json())
-      .then(d => {
-      
-          document.getElementById("app").textContent = d[0].status;
-      
+
+function afterSubmit(e){
+
+    e.preventDefault();
+
+    if (customerForm.checkValidity() === false) {
+        e.stopPropagation();
+        for(let field of customerForm.elements) {
+            if(!field.checkValidity()){
+                field.classList.add("is-invalid");
+            }
+        }
+        return;
+    }
+
+    for(let field of customerForm.elements) {
+            field.classList.remove("is-invalid");
+    }
+
+  //colunm name:html name
+    var info = {
+        이름: Name.value,
+        주민번호: Idnumber.value,
+        휴대폰번호: PhoneNumber.value,
+        주소: Address.value,
+    };
+
+    var url = "https://script.google.com/macros/s/AKfycbwNo038t8nw-MzPjWmzlhJG8zOz8lUkt1D9fEm7dQzg76n7bKPzsSoTMcLH6KVY2ZSb/exec";
+
+    buttonText.textContent = "Saving..";
+    buttonSpinner.classList.remove("d-none");
+    submitButton.disabled = true;
+
+    fetch(url,{
+        method: 'POST',
+        cache: 'no-cache',
+        redirect: 'follow',
+        body: JSON.stringify(info)
+      })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        customerForm.reset();
+        buttonText.textContent = "Send";
+        buttonSpinner.classList.add("d-none");
+        submitButton.disabled = false;
+      })
+      .catch(err => {
+        console.log(err);
+        console.log("Something Went Wrong");
+        unknownError.classList.remove("d-none");
+        setTimeout(function(){
+            unknownError.classList.add("d-none");
+            buttonText.textContent = "Send";
+            buttonSpinner.classList.add("d-none");
+            submitButton.disabled = false;
+        },3000);
       });
 
 }
 
-document.getElementById("btn").addEventListener("click", testGS);
-
-function addGS(){
-    const url = "https://script.google.com/macros/s/AKfycbzn-vjkl4BHUGJY8_k_S95cPlzVRkvyOEFOL6YoFwuX-Ryl5HSv8ixrTmqpmFjcn5Sl/exec"
-
-  fetch(url, {
-    method: 'POST',
-    mode: 'no-cors',
-    cache: 'no-cache',
-    //credentials: 'omit',
-    headers: { 
-        'Content-Type': 'application/json' 
-    },
-    redirect: 'follow',
-    body: JSON.stringify({first:"혜림",phone:"010-2222-3333",last:"심"})
-  });
-
-}
-
-document.getElementById("btn2").addEventListener("click", addGS);
+customerForm.addEventListener("submit",afterSubmit);
